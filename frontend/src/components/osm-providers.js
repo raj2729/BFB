8e269f8fb8661a9f9ad0be178a80fa2ff5de0689
Raj <rajsanghavi9@gmail.com>
2022-06-04 19:20:55 +0530

    Modifying frontend

--- frontend/src/components/osm-providers.js
+++
@@ -1,6 +0,0 @@
-export default {
-    maptiler: {
-        url: "https://api.maptiler.com/maps/basic/256/tiles.json?key=DY4gUNljkQlDsqs7aryd",
-        attribution: "https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=DY4gUNljkQlDsqs7aryd"
-    }
-};
