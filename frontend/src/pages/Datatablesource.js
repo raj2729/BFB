d469411a5cc9d6b37e7b3610b4e661b44a2b257d
raj2729 <rajsanghavi9@gmail.com>
2022-06-05 08:04:03 +0530

    Modified frontend datatable file

--- frontend/src/pages/Datatablesource.js
+++ frontend/src/pages/Datatablesource.js
@@ -7,7 +7,7 @@ export const userColumns = [
     renderCell: (params) => {
       return (
         <div className="cellwithimg">
-          <img className="cellimg" src={params.row.img} alt="avatar" />
+          {/* <img className="cellimg" src={params.row.img} alt="avatar" /> */}
           {params.row.username}
         </div>
       );
@@ -15,98 +15,98 @@ export const userColumns = [
   },
   {
     field: "email",
-    headerName: "Email",
+    headerName: "Program Enrolled",
     width: 230,
   },
   {
     field: "age",
-    headerName: "age",
+    headerName: "Total food",
     width: 230,
   },
-  {
-    field: "status",
-    headerName: "Status",
-    width: 160,
-    renderCell: (params) => {
-      return (
-        <div className={`cellwithstatus ${params.row.status} `}>
-          {params.row.status}
-        </div>
-      );
-    },
-  },
+  // {
+  //   field: "status",
+  //   headerName: "Status",
+  //   width: 160,
+  //   renderCell: (params) => {
+  //     return (
+  //       <div className={`cellwithstatus ${params.row.status} `}>
+  //         {params.row.status}
+  //       </div>
+  //     );
+  //   },
+  //},
 ];
 export const userRows = [
   {
-    id: 1,
-    username: "John Snow",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "active",
-    email: "1snow@nothing.com",
-    age: 35,
+    id: "629b75886e5c7a0b286c080b",
+    username: "NGO1",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "active",
+    email: "foodRelief",
+    age: 27,
   },
   {
-    id: 2,
-    username: "Cersei Lannister",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "pending",
-    email: "cersei@powerispower.com",
-    age: 42,
+    id: "629b76f76e5c7a0b286c080d",
+    username: "NGO2",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "pending",
+    email: "ngoFeeding",
+    age: 15,
   },
   {
-    id: 3,
-    username: "Jaime Lannister",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "passive",
-    email: "Jamie@kingslayer.com",
+    id: "629b76f76e5c7a0b286c080d",
+    username: "NGO3",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "passive",
+    email: "ngoFeeding",
     age: 45,
   },
   {
-    id: 4,
-    username: "Arya Stark",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "passive",
-    email: "aryastark@bolton.com",
-    age: 16,
+    id: "629b7fb96e5c7a0b286c080f",
+    username: "NGO4",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "passive",
+    email: "ngoFeeding",
+    age: 89,
   },
   {
-    id: 5,
-    username: "Daenerys Targaryen",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "active",
-    email: "Daenerys@ dracarys.com",
-    age: null,
+    id: "629b7fb96e5c7a0b286c080f",
+    username: "NGO5",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "active",
+    email: "ngoFeeding",
+    age: 47,
   },
   {
     id: 6,
     username: "Melisandre",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "active",
-    email: "Melisandre@gmail.com",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "active",
+    email: "foodRelief",
     age: 150,
   },
   {
     id: 7,
     username: "Ferrara",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "active",
-    email: "Clifford@gmail.com",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "active",
+    email: "foodRelief",
     age: 44,
   },
   {
     id: 8,
     username: "Frances",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "pending",
-    email: "Frances@rediffmail.com",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "pending",
+    email: "ruralAreas",
     age: 36,
   },
   {
     id: 9,
     username: "Roxie",
-    img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    status: "active",
-    email: "Roxie@gmail.com",
+    // img: "https://images.pexels.com/photos/10283734/pexels-photo-10283734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    // status: "active",
+    email: "ruralAreas",
     age: 65,
   },
 ];
