4dba4a8ea3f06c6c73d2fd7d4745e13cfce14717
Raj <rajsanghavi9@gmail.com>
2022-06-05 09:32:36 +0530

    Added routes in dataRouter

--- backend/routes/dataRouter.js
+++ backend/routes/dataRouter.js
@@ -4,100 +4,130 @@ const router = express.Router();
 const Data = require("../models/Data");
 
 router.get("/all", async (req, res) => {
-	Data.find({})
-		.populate("ngoID")
-		.populate("programID")
-		.populate("userID")
-		.exec((err, data) => {
-			if (err) {
-				res.status(400).send({
-					message: err,
-				});
-			} else {
-				res.status(200).send({
-					message: "Success",
-					data: data,
-				});
-			}
-		});
+  Data.find({})
+    .populate("ngoID")
+    .populate("programID")
+    .populate("userID")
+    .exec((err, data) => {
+      if (err) {
+        res.status(400).send({
+          message: err,
+        });
+      } else {
+        res.status(200).send({
+          message: "Success",
+          data: data,
+        });
+      }
+    });
 });
 
 router.get("/:dataid", (req, res) => {
-	Data.findById(req.params.dataid)
-		.populate("ngoID")
-		.populate("programID")
-		.populate("userID")
-		.exec((err, data) => {
-			if (err) {
-				console.log(err);
-				res.status(400).send({
-					message: err,
-				});
-			} else {
-				res.status(200).send({
-					message: "Success",
-					data: data,
-				});
-			}
-		});
+  Data.findById(req.params.dataid)
+    .populate("ngoID")
+    .populate("programID")
+    .populate("userID")
+    .exec((err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(400).send({
+          message: err,
+        });
+      } else {
+        res.status(200).send({
+          message: "Success",
+          data: data,
+        });
+      }
+    });
 });
 
 router.post("/add", (req, res) => {
-	const data = new Data({
-		ngoID: req.body.ngoID,
-		programID: req.body.programID,
-		userID: req.body.userID,
-		month: req.body.month,
-		year: req.body.year,
-		food: req.body.food,
-		children: req.body.children,
-		senior: req.body.senior,
-		female: req.body.female,
-		male: req.body.male,
-		diffabled: req.body.diffabled,
-		charitypartners: req.body.charitypartners,
-		total: req.body.total,
-	});
-	data.save((err, data) => {
-		if (err) {
-			res.status(400).send({
-				message: err,
-			});
-		} else {
-			res.status(200).send({
-				message: "Data added successfully",
-			});
-		}
-	});
+  const data = new Data({
+    ngoID: req.body.ngoID,
+    programID: req.body.programID,
+    userID: req.body.userID,
+    month: req.body.month,
+    year: req.body.year,
+    food: req.body.food,
+    children: req.body.children,
+    senior: req.body.senior,
+    female: req.body.female,
+    male: req.body.male,
+    diffabled: req.body.diffabled,
+    charitypartners: req.body.charitypartners,
+    total: req.body.total,
+  });
+  data.save((err, data) => {
+    if (err) {
+      res.status(400).send({
+        message: err,
+      });
+    } else {
+      res.status(200).send({
+        message: "Data added successfully",
+      });
+    }
+  });
 });
 
 router.post("/:dataid", (req, res) => {
-	const data = new Data({
-		ngoID: req.body.ngoID,
-		programID: req.body.programID,
-		userID: req.body.userID,
-		month: req.body.month,
-		year: req.body.year,
-		food: req.body.food,
-		children: req.body.children,
-		senior: req.body.senior,
-		female: req.body.female,
-		male: req.body.male,
-		diffabled: req.body.diffabled,
-		charitypartners: req.body.charitypartners,
-		total: req.body.total,
-	});
-	Data.findByIdAndUpdate(req.params.dataid, data, (err, data) => {
-		if (err) {
-			res.status(400).send({
-				message: err,
-			});
-		} else {
-			res.status(200).send({
-				message: "Data updated successfully",
-			});
-		}
-	});
+  const data = new Data({
+    ngoID: req.body.ngoID,
+    programID: req.body.programID,
+    userID: req.body.userID,
+    month: req.body.month,
+    year: req.body.year,
+    food: req.body.food,
+    children: req.body.children,
+    senior: req.body.senior,
+    female: req.body.female,
+    male: req.body.male,
+    diffabled: req.body.diffabled,
+    charitypartners: req.body.charitypartners,
+    total: req.body.total,
+  });
+  Data.findByIdAndUpdate(req.params.dataid, data, (err, data) => {
+    if (err) {
+      res.status(400).send({
+        message: err,
+      });
+    } else {
+      res.status(200).send({
+        message: "Data updated successfully",
+      });
+    }
+  });
+});
+
+router.get("/children", (req, res) => {
+  Data.find({}, "_id children", (err, data) => {
+    if (err) {
+      res.status(400).send({
+        message: err,
+      });
+    } else {
+      res.status(200).send({
+        message: "Success",
+        data: data,
+      });
+    }
+  });
+});
+
+router.get("/female", (req, res) => {
+  Data.find({}, "_id female", (err, data) => {
+    if (err) {
+      res.status(400).send({
+        message: err,
+      });
+    } else {
+      res.status(200).send({
+        message: "Success",
+        data: data,
+      });
+    }
+  });
 });
 
 module.exports = router;
